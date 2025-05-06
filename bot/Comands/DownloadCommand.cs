a5a37a7879b4af75523504a31e58889b010e740a
gandonio1337 <x7482784@gmail.com>
2025-05-06 17:15:14 +0300

    Update DownloadCommand.cs

--- bot/Comands/DownloadCommand.cs
+++ bot/Comands/DownloadCommand.cs
@@ -1,1 +1,27 @@
+using Discord;
+using Discord.WebSocket;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace MyApp.bot.Commands
+{
+    public class DownloadCommand
+    {
+        public async Task ExecuteAsync(SocketSlashCommand command)
+        {
+            var latestFile = Directory.GetFiles("loader_builds")
+                .Select(f => new FileInfo(f))
+                .OrderByDescending(f => f.CreationTime)
+                .FirstOrDefault();
+
+            if (latestFile == null)
+            {
+                await command.RespondAsync("No loader builds found.", ephemeral: true);
+                return;
+            }
+
+            await command.RespondWithFileAsync(latestFile.FullName, "Here is the latest loader build:");
+        }
+    }
+}
